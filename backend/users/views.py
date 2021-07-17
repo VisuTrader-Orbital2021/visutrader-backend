34a6b8bd83f9620f71a8dc191e013eb214153d3b
DystoriaX <dascogabriel@gmail.com>
2021-07-17 22:35:26 +0800

    Add user permissions

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,8 +1,10 @@
 from rest_framework import generics
 from users.models import UserAccount
+from users.permissions import IsUser, IsSuperuser
 from users.serializers import UserSerializer
 
 # Create your views here.
 class UserDetailsView(generics.RetrieveAPIView):
     queryset = UserAccount.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsSuperuser | IsUser]
